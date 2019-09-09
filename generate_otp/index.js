e679568db57b208e97d89bc2890d3e6028b50c21
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2019-09-09 16:47:58 +0530

    added otp generation hint

--- generate_otp/index.js
+++ generate_otp/index.js
@@ -27,7 +27,7 @@ if (phone && name && password) {
             if (body.success) {
                 hintBox = document.getElementById('hint')
                 hintBox.innerHTML = `
-                    <a href='tusharsadhwani.github.io/invento-referrals/'>
+                    <a href='https://tusharsadhwani.github.io/invento-referrals/'>
                         Click Here
                     </a>
                     to continue creating your referral
